629c5f410f0c2d2fcbd3bed0cba956253aa18cd7
aakashmondal6 <aakashmondal6@gmail.com>
2025-07-06 00:33:36 +0530

    implemented CORS in backend
    implemented CORS in backend

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,15 @@ const bodyParser = require('express').json;
 const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const employeeRoutes = require('./routes/employeeRoutes');
+const cors = require('cors');
+
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'DELETE']
+})); // Enable CORS for all routes
 app.use(bodyParser());
 app.use(logger);
 app.use('/api/employees', employeeRoutes);
